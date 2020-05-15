ecf76b71e28d0ce734d2d21725fb04254ea4f7ab
Harley Wiltzer <harley.wiltzer@mail.mcgill.ca>
2020-05-14 22:03:51 -0400

    Adds mark read/unread functionality

--- app/controllers/LibraryController.scala
+++ app/controllers/LibraryController.scala
@@ -13,7 +13,7 @@ import io.github.harwiltz.sagerank._
 @Singleton
 class LibraryController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  type Message = (Int, String)
+  val libraryStatusColumns: Array[String] = State.statusesStr.dropRight(1)
 
   /**
    * Create an Action to render an HTML page.
@@ -26,11 +26,41 @@ class LibraryController @Inject()(val controllerComponents: ControllerComponents
     val requestParams = request.queryString.map { case (k, v) => (k -> v.mkString) }
     val statusCode: Int = requestParams.get("status").map(_.toInt).getOrElse(1) // Default to "To-read" column
     val library: Iterable[ArticleMetadata] = getLibrary(State.decodeArticleStatus(statusCode))
-//    val library: Iterable[ArticleMetadata] = statusCode.map(code => State.decodeArticleStatus(code))
-//                                                       .map(status => getLibrary(status))
-//                                                       .getOrElse(Iterable[ArticleMetadata]())
-//    val idx = statusCode.map(code => code.toInt).getOrElse(1) // Default to "To Read" column
-    Ok(views.html.library(statusCode, State.statusesStr, library))
+    Ok(views.html.library(statusCode, libraryStatusColumns, library))
+  }
+
+  def updateStatus() = Action { implicit request: Request[AnyContent] =>
+    val requestParams = request.queryString.map { case (k, v) => (k -> v.mkString) }
+    val statusCode: Int = requestParams.get("status").map(_.toInt).getOrElse(1) // Default to "To-read" column
+    val statusDeltaCode: Int = requestParams.get("statusDelta").map(_.toInt).getOrElse(0)
+    val articleId: Option[String] = requestParams.get("articleid")
+    val articleMetadata: Option[ArticleMetadata] = articleId.map { id =>
+      ArticleMetadata(id,
+                      "",
+                      Vector[String](),
+                      "",
+                      "",
+                      "",
+                      UnreadArticle)
+    }
+    val artbib = articleMetadata.map(am => ArticleBibliography(am, Vector[ArticleBibliography]()))
+
+    val articleMessage = requestParams.get("status").flatMap { statusCode =>
+      val status = State.decodeArticleStatus(statusCode.toInt + statusDeltaCode)
+      artbib.map { a =>
+        val title = State.graph.articleMap.get(a.article.id).map(x => x.article.title)
+        val forceChange = statusDeltaCode > 0
+        State.withSaveGraph(State.graphPath) {
+          State.graph = State.graph.withChangedStatus(status, forceChange)(a)
+        }
+        title match {
+          case None => (-1, "Couldn't find requested article in library.")
+          case Some(t) => (1, s"Expanded citation tree for '${t}'.")
+        }
+      }
+    }
+    val library: Iterable[ArticleMetadata] = getLibrary(State.decodeArticleStatus(statusCode))
+    Ok(views.html.library(statusCode, libraryStatusColumns, library))
   }
 
   def getLibrary(status: ArticleStatus): Iterable[ArticleMetadata] = State.graph.articleMap.filter { case(id, artbib) =>
